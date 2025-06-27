685f63a5df5b2356094a55203fafbaeb8dfafabf
Chris Kemeza <ckemeza1@gmail.com>
2025-06-27 17:02:15 -0400

    removed accidental tailwind .css file watching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.liquid',
-    './**/*.json',
-    './**/*.css'
+    './**/*.liquid'
   ],
   theme: {
     extend: {
